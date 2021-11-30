73e9f80822bf992c4605d7fecccf0aa683c20f49
Cristian Diego Góngora Pabón <70243474+eudemonologia@users.noreply.github.com>
2021-11-29 23:18:52 -0300

    Proyecto terminado en Node y Handlebars con conexión a la base de datos MySQL.

--- routes/contacto.js
+++ routes/contacto.js
@@ -1,11 +1,39 @@
 var express = require("express");
 var router = express.Router();
+var nodemailer = require("nodemailer");
 
 router.get("/", function (req, res, next) {
   res.render("contacto", {
     title: "Contacto",
     contacto: true,
     user: req.session.user,
+  });
+});
+
+router.post("/", async (req, res, next) => {
+  console.log(req.body);
+  const mail = {
+    to: "cristo.ottis@gmail.com",
+    subject: "Contacto desde la web Transporte X",
+    html: `${req.body.name} <br> email: ${req.body.email} <br> teléfono: ${req.body.tel} <br> ha enviado el siguiente mensaje: <br> ${req.body.comment} `,
+  };
+
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  });
+
+  await transporter.sendMail(mail);
+
+  res.render("contacto", {
+    title: "Contacto",
+    contacto: true,
+    user: req.session.user,
+    mensaje: "Mensaje enviado correctamente",
   });
 });
 
